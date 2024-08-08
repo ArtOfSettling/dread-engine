e436242380ee7ffe9fe8dd6f45e5b3b88b18df6a
Terry Magnus Drever <github.snowbird189@passmail.net>
2024-08-07 18:53:37 -0700

    Include constants for all levels.

--- src/constants/application.rs
+++ src/constants/application.rs
@@ -1,1 +1,4 @@
 pub(crate) const APPLICATION_TITLE: &str = "Dread Engine";
+pub(crate) const APPLICATION_WORKING_DIRECTORY: &str = "";
+pub(crate) const APPLICATION_WIDTH: u32 = 1600;
+pub(crate) const APPLICATION_HEIGHT: u32 = 900;
